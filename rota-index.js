2f7a4fae8447a2403ea4021003d3c21d133e6e17
dmgriff <128525255+dmgriff@users.noreply.github.com>
2026-05-07 13:08:00 +0100

    Add new entry for April - August 2026

--- rota-index.js
+++ rota-index.js
@@ -1,3 +1,4 @@
 window.ROTA_INDEX = [
-  { key: "aug-nov-2026", label: "August - November 2026", file: "data/aug-nov-2026.js" }
+  { key: "aug-nov-2026", label: "August - November 2026", file: "data/aug-nov-2026.js" },
+  { key: "apr-aug-2026", label: "April - August 2026", file: "data/apr-aug-2026.js" }
 ];
